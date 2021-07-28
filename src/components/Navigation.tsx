154a8792f8085e2e09d8cce5c38e5b84c65661af
Joaquin Abregu <abregujoaquin@gmail.com>
2021-07-28 17:55:44 -0300

    initial commit

---
+++ src/components/Navigation.tsx
@@ -0,0 +1,21 @@
+import { Link } from "react-router-dom";
+import logo from "./logo.png";
+function Navigation() {
+  return (
+    <header className="header">
+      <nav>
+        <img src={logo} alt="logo" />
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/catalogo">Catalogo</Link>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
+
+export default Navigation;
