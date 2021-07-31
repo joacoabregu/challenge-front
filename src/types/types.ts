ab492a035b0a4e5e4c71041fd7608811cf314d26
Joaquin Abregu <abregujoaquin@gmail.com>
2021-07-31 11:20:10 -0300

    ItemsState

--- src/types/types.ts
+++ src/types/types.ts
@@ -1,4 +1,11 @@
+import { Items } from "./interfaces";
+
 export type Inputs = {
   email: string;
   message: string;
+};
+export type ItemsState = {
+  status: "loading" | "idle" | "error";
+  error: string | null;
+  items: Items;
 };
