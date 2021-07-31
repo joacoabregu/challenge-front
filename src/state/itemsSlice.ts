255423a719adadebf5da740bad6c096f6dd18f16
Joaquin Abregu <abregujoaquin@gmail.com>
2021-07-31 16:24:38 -0300

    cleanup

--- src/state/itemsSlice.ts
+++ src/state/itemsSlice.ts
@@ -1,22 +1,11 @@
 import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 import axios, { AxiosResponse } from "axios";
 import type { RootState } from "./store";
-import { Item, Items } from "../types/interfaces";
+import { Items } from "../types/interfaces";
+import { ItemsState } from "../types/types";
 
-
-
-type ItemsState = {
-  // In `status` we will watch
-  // if todos are being loaded.
-  status: "loading" | "idle" | "error";
-
-  // `error` will contain an error message.
-  error: string | null;
-  items: Items;
-};
 const initialState = {
   items: [],
-  error: null,
   status: "idle",
 } as ItemsState;
 
@@ -24,7 +13,6 @@ export const getItems = createAsyncThunk<Items>("items/getItems", async () => {
   const response: AxiosResponse = await axios.get(
     "https://rooftop-api-rest-frontend.herokuapp.com/items"
   );
-
   let data: Items = await response.data.items;
   return data;
 });
@@ -36,7 +24,6 @@ export const itemsSlice = createSlice({
   extraReducers: (builder) => {
     builder.addCase(getItems.fulfilled, (state, { payload }) => {
       state.status = "idle";
-
       state.items.push(...payload);
     });
     builder.addCase(getItems.rejected, (state, { payload }) => {
@@ -48,7 +35,6 @@ export const itemsSlice = createSlice({
   },
 });
 
-//export const { increment } = itemsSlice.actions;
 // Other code such as selectors can use the imported `RootState` type
 export const selectItems = (state: RootState) => state.items.items;
 export const selectStatus = (state: RootState) => state.items.status;
