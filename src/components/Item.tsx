8dc7aeef05f5f15526c8288ef570196f4e1aba9b
Joaquin Abregu <abregujoaquin@gmail.com>
2021-08-01 13:24:05 -0300

    link

--- src/components/Item.tsx
+++ src/components/Item.tsx
@@ -1,5 +1,6 @@
 import { ItemProps } from "../types/types";
 import "../styles/Item.css";
+import { Link } from "react-router-dom";
 export default function Item({ item }: ItemProps) {
   let price;
   if (item.offer) {
@@ -7,16 +8,19 @@ export default function Item({ item }: ItemProps) {
   } else {
     price = item.price;
   }
+  let url = "/catalogo/detalle/" + item.id;
   return (
-    <div key={item.id} className="item">
-      <img src={item.images[0]} alt="product"></img>
-      <div>
-        <p className="item-title">{item.title}</p>
-        <p>
-          {item.currency}
-          {price}
-        </p>
+    <Link to={url}>
+      <div key={item.id} className="item">
+        <img src={item.images[0]} alt="product"></img>
+        <div>
+          <p className="item-title">{item.title}</p>
+          <p>
+            {item.currency}
+            {price}
+          </p>
+        </div>
       </div>
-    </div>
+    </Link>
   );
 }
