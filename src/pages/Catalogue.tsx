e951bc8c8104bb64c922d9954b6477d9d8048f3e
Joaquin Abregu <abregujoaquin@gmail.com>
2021-08-02 17:47:24 -0300

    pagination

--- src/pages/Catalogue.tsx
+++ src/pages/Catalogue.tsx
@@ -2,9 +2,10 @@ import { useAppSelector, useAppDispatch } from "../state/hooks";
 import { getItems, selectItems, selectStatus } from "../state/itemsSlice";
 import { Item as ItemInterface } from "../types/interfaces";
 import { Items } from "../types/interfaces";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import Item from "../components/Item";
 import Spinner from "../components/Spinner";
+import Pagination from "../components/Pagination";
 
 export default function Catalogue() {
   let items: Items = useAppSelector(selectItems);
@@ -21,6 +22,15 @@ export default function Catalogue() {
     }
   }, [status, dispatch]);
 
+  const [currentPage, setCurrentPage] = useState(1);
+  const [booksPerPage] = useState(5);
+  // Get current books
+  const indexOfLastBook = currentPage * booksPerPage;
+  const indexOfFirstBook = indexOfLastBook - booksPerPage;
+  const currentItems = items.slice(indexOfFirstBook, indexOfLastBook);
+  // Change page
+  const paginate = (pageNumber: number) => setCurrentPage(pageNumber);
+
   if (status === "error") {
     return (
       <div>
@@ -34,9 +44,15 @@ export default function Catalogue() {
   } else {
     return (
       <>
-        {items.map((data: ItemInterface, index) => (
+        {currentItems.map((data: ItemInterface, index) => (
           <Item item={data} key={index} />
         ))}
+        <Pagination
+          perPage={booksPerPage}
+          total={items.length}
+          paginate={paginate}
+          currentPage={currentPage}
+        />
       </>
     );
   }
