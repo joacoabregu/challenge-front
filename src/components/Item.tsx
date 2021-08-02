48cf6cfeabfa6f11f809a7e11687fac60b912b18
Joaquin Abregu <abregujoaquin@gmail.com>
2021-08-02 13:14:41 -0300

    date Difference

--- src/components/Item.tsx
+++ src/components/Item.tsx
@@ -2,12 +2,18 @@ import { ItemProps } from "../types/types";
 import "../styles/Item.css";
 import { Link } from "react-router-dom";
 import "../styles/Item.css";
+import { dateDifference } from "../helpers/functions";
 export default function Item({ item }: ItemProps) {
   let price: string;
   let priceClass: string = "";
+  let daysOffer: number = 0;
+  let hoursOffer: number = 0;
   if (item.offer) {
     price = item.offer.price.toString();
     priceClass = "item-price--alert";
+    let differenceOffer = dateDifference(item.offer?.expires_at);
+    daysOffer = differenceOffer.days;
+    hoursOffer = differenceOffer.hours;
   } else {
     price = item.price;
   }
@@ -15,11 +21,14 @@ export default function Item({ item }: ItemProps) {
   return (
     <div className="item" key={item.id}>
       <img src={item.images[0]} alt="product"></img>
-      <div>
+      <div className="item-text">
         <p className="item-title">{item.title}</p>
         <p
           className={`item-price ${priceClass}`}
         >{`${item.currency} ${price}`}</p>
+        {item.offer && (
+          <p>{`Esta oferta finaliza en ${daysOffer.toString()} días, ${hoursOffer.toString()} horas`}</p>
+        )}
         <Link to={url}>Ver más</Link>
       </div>
     </div>
--- src/helpers/functions.ts
+++ src/helpers/functions.ts
@@ -1,6 +1,17 @@
+import dayjs from "dayjs";
+
 export function commentToDate(data: string): string {
   let str: string = data;
   let strArr: string[] = str.slice(0, 10).split("-");
   let date: string = strArr[2] + "-" + strArr[1] + "-" + strArr[0];
   return date;
+}
+
+export function dateDifference(date: string): { days: number; hours: number } {
+  let offerExpiration = dayjs(date);
+  let now = dayjs();
+  let dateDifference = offerExpiration.diff(now, "day", true);
+  let days = Math.floor(dateDifference);
+  let hours = Math.floor((dateDifference - days) * 24);
+  return { days, hours };
 }
