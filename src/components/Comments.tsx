765b6f271eb3d442c2d9bd7143c89e326ff04693
Joaquin Abregu <abregujoaquin@gmail.com>
2021-08-03 13:38:12 -0300

    navigation

--- src/components/Comments.tsx
+++ src/components/Comments.tsx
@@ -2,32 +2,68 @@ import Comment from "./Comment";
 import { CommentsProps } from "../types/types";
 import { Comment as CommentInterface } from "../types/interfaces";
 import { useEffect, useState } from "react";
-import axios, { AxiosResponse } from "axios";
+import "../styles/Comments.css";
+import { useAppSelector, useAppDispatch } from "../state/hooks";
+import {
+  getComments,
+  selectComments,
+  selectStatus,
+} from "../state/commentsSlice";
+import { Comments as CommentsInterface } from "../types/interfaces";
+import Spinner from "./Spinner";
+import { getCurrentPagination } from "../helpers/functions";
+import Pagination from "./Pagination";
 
 export default function Comments({ id }: CommentsProps) {
-  let [comments, setComments] = useState<CommentInterface[] | []>([]);
-  const url: string =
-    "https://rooftop-api-rest-frontend.herokuapp.com/questions";
+  let comments: CommentsInterface = useAppSelector(selectComments);
+  const dispatch = useAppDispatch();
   useEffect(() => {
-    let urlGET: string = url + "?item_id=" + id;
-    axios
-      .get(urlGET)
-      .then((response: AxiosResponse) => {
-        setComments(response.data);
-      })
-      .catch((error) => {
-        console.log(error);
-      });
-  }, [id]);
+    dispatch(getComments(id));
+  }, [dispatch, id]);
+
+  let status = useAppSelector(selectStatus);
+  useEffect(() => {
+    if (status === "error") {
+      dispatch(getComments(id));
+    }
+  }, [status, dispatch, id]);
+
+  const [currentPage, setCurrentPage] = useState(1);
+  const commentsPerPage: number = 4;
+  const currentComments = getCurrentPagination(
+    comments,
+    currentPage,
+    commentsPerPage
+  );
+  const paginate = (pageNumber: number): void => setCurrentPage(pageNumber);
+
+  if (status === "error") {
+    return (
+      <div>
+        <p>Se ha producido un error. Intente más tarde</p>
+      </div>
+    );
+  }
+  if (status === "loading") {
+    return <Spinner />;
+  }
 
   if (!comments.length) {
     return <p>No hay comentarios</p>;
   } else {
     return (
       <>
-        {comments.map((data: CommentInterface) => (
-          <Comment comment={data} />
-        ))}
+        <div className="comments">
+          {currentComments.map((data: CommentInterface) => (
+            <Comment comment={data} key={data.sent_at} />
+          ))}
+        </div>
+        <Pagination
+          perPage={commentsPerPage}
+          total={comments.length}
+          paginate={paginate}
+          currentPage={currentPage}
+        />
       </>
     );
   }
