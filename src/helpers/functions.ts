bbf38fcc45852f1228274424371914548d18141c
Joaquin Abregu <abregujoaquin@gmail.com>
2021-08-05 16:37:35 -0300

    comments

--- src/helpers/functions.ts
+++ src/helpers/functions.ts
@@ -1,12 +1,31 @@
 import dayjs from "dayjs";
 
-export function commentToDate(data: string): string {
+/**
+ * Receives date as ex: 2021-07-24T07:36:44.652Z
+ * Returns comment date as DD-MM-YYYY.
+ *
+ * @param {string} x Date
+ * @return {string} x Date.
+ */
+export function commentDateFormat(data: string): string {
   let str: string = data;
+  // Remove second part of string ex: T07:36:44.652Z and then split date
   let strArr: string[] = str.slice(0, 10).split("-");
+  // Rearrange string date to DD-MM-YYYY
   let date: string = strArr[2] + "-" + strArr[1] + "-" + strArr[0];
   return date;
 }
 
+/**
+ * Calculates difference between two dates
+ * Receives date as ex: 2021-07-24T07:36:44.652Z
+ * Returns comment date as
+ * "Esta oferta finaliza en X días" if hours is less than 1
+ * "Esta oferta finaliza en X días y 1 hora" if hours is equal to 1
+ * "Esta oferta finaliza en X días y X horas" if hours is more than 1
+ * @param {string} x Date
+ * @return {string} x Date.
+ */
 export function dateDifferenceToStr(date: string): string {
   let offerExpiration = dayjs(date);
   let now = dayjs();
